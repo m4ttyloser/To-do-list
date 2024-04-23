63c5d7508d9937edcbfa0132410e0f6cbd834471
Ismat Emir <misterismatemir@gmail.com>
2024-04-24 01:51:54 +0800

    Completed draft of this feature but without error handling

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-while True:
-    todo_list = []
+todo_list = []
 
+while True:
+    print('+----------------------------+')
     print('1.Add task')
     print('2.Remove task')
     print('3.Show task')
-    print('Quit program')
+    print('4.Quit program')
+    print('+----------------------------+')
 
     pick = int(input('Your action:'))
 
     if pick == 1:
-        task = input('Enter task:')
+        task = input('Enter task:').lower()
         todo_list.append(task)
     
     elif pick == 2:
@@ -19,4 +21,23 @@ while True:
             print(f'{y}.{i}')
             y += 1
         
-        remove = int(input('Whats the number of task you want to remove:'))
+        remove = input('What task do you want to remove:').lower()
+
+        if remove in todo_list:
+            todo_list.remove(remove)
+            print('task removed')
+        else:
+            print('Task not found!')
+
+    elif pick == 3:
+        z = 1
+        for x in todo_list:
+            print(f'{z}.{x}')
+            z += 1
+
+    elif pick == 4:
+        print('Exiting')
+        break
+
+    else:
+        print('Number is not recognized')
