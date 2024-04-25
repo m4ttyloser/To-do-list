820f4b29b2d35adbac77d2ffe30341b66aa78eba
Ismat Emir <misterismatemir@gmail.com>
2024-04-26 03:25:12 +0800

    !

--- main.py
+++ main.py
@@ -5,26 +5,25 @@ import time
 
 class todolistFeat:
     def __init__(self):
-        self.task = []
+        self.tasks = []
         self.total_points = 0
 
     def add_task(self, task, points=0, deadline = None):
         timestamp = datetime.now().strftime('HH:MM AM/PM')
         self.task.append({"task" : task, "points": points, "deadline": deadline, "timestamp": timestamp, "status": False})
-        
-
-    def remove_task(todo_list, task):
-        if remove in todo_list:
-            todo_list.remove(remove)
-            print('task removed')
+    
+    
+    def remove_task(self, task_index):
+        if 0 <= task_index < len(self.tasks):
+            del self.tasks[task_index]
+            self.refresh_task()
         else:
-            print('Task not found!')
+            messagebox.showerror("Error","Please pick  a task")
+    
 
-
-    def show_task(todo_list):
-        print('Your task:')
-        z = 1
-        for x in todo_list:
-            print(f'{z}.{x}')
-            z += 1
-
+    def mark_as_done(self,task_index):
+        if 0 <= task_index < len(self.tasks):
+            self.tasks[task_index]["done"] = True
+            self.total_points += self.tasks[task_index]["points"]
+        else:
+            messagebox.showerror("Error", "Please pick a task")
