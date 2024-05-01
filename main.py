f5890f5a2691fd9c1bdb5c49008fce9426c72acb
Ismat Emir <misterismatemir@gmail.com>
2024-05-02 01:43:31 +0800

    Add task and remove task feature completed\

--- main.py
+++ main.py
@@ -1,55 +1,45 @@
 from tkinter import *
 from tkinter import messagebox
-from datetime import datetime
-import time
 
-class todolistFeat:
-    def __init__(self):
-        self.tasks = []
-        self.total_points = 0
+def add_task():
+    task_text = task_entry.get()
+    if task_text:
+        listbox.insert(END, task_text)
+        task_entry.delete(0, END)
+    else:
+        messagebox.showwarning("Warning", "Please enter a task.")
 
-    def add_task(self, task, points=0, deadline = None):
-        timestamp = datetime.now().strftime('HH:MM AM/PM')
-        self.task.append({"task" : task, "points": points, "deadline": deadline, "timestamp": timestamp, "status": False})
-    
-    
-    def remove_task(self, task_index):
-        if 0 <= task_index < len(self.tasks):
-            del self.tasks[task_index]
-            self.refresh_task()
-        else:
-            messagebox.showerror("Error","Please pick  a task")
-    
 
-    def mark_as_done(self,task_index):
-        if 0 <= task_index < len(self.tasks):
-            self.tasks[task_index]["done"] = True
-            self.total_points += self.tasks[task_index]["points"]
-        else:
-            messagebox.showerror("Error", "Please pick a task")
+def remove_task():
+    try:
+        selected_task_index = listbox.curselection()[0]
+        listbox.delete(selected_task_index)
+    except IndexError:
+        pass
 
 
 root = Tk()
-root.title("TO-do list")
+root.title("To-do list")
+root.geometry("400x650+400+100")
 root.resizable(False,False)
 
 task_list = []
 
 #icon
-Image_icon = PhotoImage(file = "image/task.png") # image not popping out
+Image_icon = PhotoImage(file = "task.png") # image not popping out
 root.iconphoto(False,Image_icon)
 
-TopImage = PhotoImage(file = "image/tobar.png")
+TopImage = PhotoImage(file = "topbar.png")
 Label(root,image = TopImage).pack()
 
-dockImage = PhotoImage(file ="image/dock.png")
+dockImage = PhotoImage(file ="dock.png")
 Label(root,image = dockImage,bg="#32405b").place(x=30,y=25)
 
-noteImage = PhotoImage(file = "image/task.png")
+noteImage = PhotoImage(file = "task.png")
 Label(root,image = noteImage,bg="#32405b").place(x=30,y=25)
 
-heading = Label(root,text="ALL TASK",font = "arial 20 bold",fg="white",bg="#32405b")
-heading.lace(x=130,y=20)
+heading = Label(root,text="PLANNER",font = "arial 20 bold",fg="white",bg="light blue")
+heading.place(x=130,y=20)
 
 #main
 frame = Frame(root,width=400, height = 50,bg = "white")
@@ -59,24 +49,42 @@ task = StringVar()
 task_entry = Entry(frame,width = 18,font = "arial 20",bd=0)
 task_entry.place(x = 10, y = 7)
 
-
-button = Button(frame,text = "ADD",font = "arial 20 bold", width = 6, bg = "5a95ff",fg = "#fff", bd =0)
+#add task button
+button = Button(frame,text = "+",font = "arial 20 bold", width = 6, bg = "light blue",fg = "black", bd =0, command = add_task)
 button.place(x=300, y= 0)
 
 #listbox
-frame1 = Frame(root,bd=3,width=700,height=280,bg="32405b")
+frame1 = Frame(root,bd=3,width=700,height=280,bg="blue")
 frame1.pack(pady=(160,0))
 
-listbox = Listbox(frame1, font=("arial",12),width=40,height=16,bg="#32405b",fg="white",cursor="hand2",selectbackground="#5a95ff")
+#task list
+listbox = Listbox(frame1, font=("arial",12),width=40,height=16,bg="pink",fg="black",cursor="hand2",selectbackground="black")
 listbox.pack(side = LEFT, fill=BOTH,padx=2)
 scrollbar = Scrollbar(frame1)
 scrollbar.pack(side=RIGHT,fill =BOTH)
 listbox.config(yscrollcommand=scrollbar.set)
 scrollbar.config(command=listbox.yview)
 
-#delete
-delete_icon = PhotoImage(file = "image/delete.png")
-Button(root,image=delete_icon,bd=0).pack(side=BOTTOM,pady=13)
+#delete button
+delete_icon = PhotoImage(file="delete.png")
+Button(root, image=delete_icon, bd=0, command=remove_task).pack(side=LEFT,padx = 50)
+
+
+#mark as done button
+mark_as_done = PhotoImage(file="tick.png")
+small_mark_as_done = mark_as_done.subsample(3, 3)  
+Button(root, image=small_mark_as_done, bd=0).pack(side=RIGHT, pady=10, padx = 50)
+
+#pomodoro button
+pomodoro_icon = PhotoImage(file="timer icon.png")
+small_pomodoro_icon = pomodoro_icon.subsample(6, 6)
+Button(root, image=small_pomodoro_icon, bd=0,).place(x=60, y= 90)
+
+#Progress button
+progress_icon = PhotoImage(file="progress.png")
+small_progress_icon = progress_icon.subsample(6, 6)
+Button(root, image=small_progress_icon, bd=0,).place(x=240, y= 90)
+
 
 
 
